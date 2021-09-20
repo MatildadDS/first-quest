8a2bae5b3231a7a65767d3a4cd45e30e040a2f59
Matildad <mtld.desouza@gmail.com>
2021-09-20 11:24:40 +0200

    correction:hammer:

--- typescript-basic/challenge.ts
+++ typescript-basic/challenge.ts
@@ -4,23 +4,18 @@ interface User {
     age: number;
 }
 
-type wilders = {
-    name: string;
-    age: number;
-}
 
-
-const prettyPrintWilder = (users) => {
+const prettyPrintWilder = (users: User[]) => {
     users.map((user) => {
       console.log(`${user.name} is ${user.age} years old`);
     });
   };
 
   
-  const wilders = [];
-  const user1 = { name: "Pierre", age: 23 };
-  const user2 = { name: "Paul", age: 31 };
-  const user3 = { name: "Jacques", age: 25 };
+  const wilders: User[] = [];
+  const user1 : User = { name: "Pierre", age: 23 };
+  const user2 : User = { name: "Paul", age: 31 };
+  const user3 : User = { name: "Jacques", age: 25 };
   wilders.push(user1);
   wilders.push(user2);
   wilders.push(user3);
